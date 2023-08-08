2a7e8550c4f6ec23574c12b7426ea25f7a4b6016
Yakup Arslan <132354732+YakupAr@users.noreply.github.com>
2023-08-08 19:15:53 +0300

    Update date-checker.py

--- date-checker/date-checker.py
+++ date-checker/date-checker.py
@@ -1,16 +1,15 @@
-## Date checker to check if date is valid
 def date_checker(date):
     try:
-        year = int(date[4:8])
+        day = int(date[0:2])
         month = int(date[2:4])
-        day = int(date[:2])
+        year = int(date[4:8])
         
-        if year < 1 or month < 1 or month > 12 or date < 1:
+        if year < 1 or month < 1 or month > 12 or day < 1:
             return False
         
         month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
         
-# Check for leap year and adjust February's day count if needed
+        # Check for leap year and adjust February's day count if needed
         if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
             month_days[1] = 29
 
@@ -22,7 +21,6 @@ def date_checker(date):
         return False
 
 date = input("Date (DDMMYYYY): ")
-
 
 # Check if the provided date is valid using the function
 if date_checker(date):
